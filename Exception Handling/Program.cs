d8cd7c8d3b63d6087e1b1721dfff018272b1ae52
Jacob <hillj@stpaulsschool.org.uk>
2020-10-13 12:23:29 +0100

    Add project files.

---
+++ Exception Handling/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SafeSquareRoot(100);
+            SafeSquareRoot(-100);
+            Console.Read();
+        }
+
+        static void SafeSquareRoot(int i)
+        {
+            if (i < 0)
+            {
+                Console.WriteLine("Invalid Number");
+            }
+            else
+            {
+                Console.WriteLine("")
+            }
+        }
+
+
+    }
+}
